d3837dfcab2a83c2763bcb359e54256c3823a2e2
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2026-01-28 18:39:19 +0100

    chore: minor platform

---
+++ .changeset/hot-weeks-repair.md
@@ -0,0 +1,5 @@
+---
+"@ivan-angelkoski/counter": patch
+---
+
+make the package platform neutral
--- packages/counter/tsdown.config.ts
+++ packages/counter/tsdown.config.ts
@@ -4,8 +4,10 @@ export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "dist",
   format: ["esm", "cjs"],
-  sourcemap: true,
   dts: true,
   clean: true,
   target: ["es2022"],
+  minify: true,
+  // if this is set to node, the module js files are with a .mjs prefix instead of .js
+  platform: "browser",
 });
